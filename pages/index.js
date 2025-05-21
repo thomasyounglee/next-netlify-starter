1c9e58feefe8519088b2c01a85b40a57376d7e9d
thomasyounglee <109660741+thomasyounglee@users.noreply.github.com>
2025-05-21 22:30:21 +0900

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
         <Header title="Welcome to Alarm by Date!" />
         
         {/* 이미지를 텍스트 아래로 이동 */}
-        <img src="/test.jpg" alt="테스트 이미지" />
+        //<img src="/test.jpg" alt="테스트 이미지" />
         
         <p className="description">
           Get started by editing <code>pages/index.js</code>
