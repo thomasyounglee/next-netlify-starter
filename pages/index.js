9446760034d50296b2d9b2289c05c0e623c21a25
thomasyounglee <109660741+thomasyounglee@users.noreply.github.com>
2025-05-21 22:05:38 +0900

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
     <div className="container">
       <Head>
         <title>Alarm by Date!</title>
-        //<link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
